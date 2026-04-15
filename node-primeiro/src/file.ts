ef4039770ff595a2470bc4888dfcd73ed5c50284
Flávio Félix <fau_felix30@hotmail.com>
2026-04-15 18:46:45 -0300

    Alterando o arquivo txt

--- node-primeiro/src/file.ts
+++ node-primeiro/src/file.ts
@@ -1,9 +1,13 @@
 import { readFile, writeFile } from "fs/promises";
 
 const exec = async () => {
-  const fileContent = await readFile("./teste.txt", "utf-8");
+  const fileName = "./teste.txt";
+  const fileContent = await readFile(fileName, "utf-8");
+
   const list = fileContent.split("\n");
-  console.log(list);
+  list.push("Luciana");
+  const listTxt = list.join("\n");
+  await writeFile(fileName, listTxt);
 };
 
 exec();
