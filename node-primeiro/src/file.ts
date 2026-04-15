8201b321fd52e7d13c96c6870545a0a6caaa7760
Flávio Félix <fau_felix30@hotmail.com>
2026-04-15 19:04:13 -0300

    Deletando arquivo txt

--- node-primeiro/src/file.ts
+++ node-primeiro/src/file.ts
@@ -1,13 +1,14 @@
-import { readFile, writeFile } from "fs/promises";
+import { readFile, unlink, writeFile } from "fs/promises";
 
 const exec = async () => {
-  const fileName = "./teste.txt";
+  //await unlink("./teste.txt");
+  /* const fileName = "./teste.txt";
   const fileContent = await readFile(fileName, "utf-8");
 
   const list = fileContent.split("\n");
   list.push("Luciana");
   const listTxt = list.join("\n");
-  await writeFile(fileName, listTxt);
+  await writeFile(fileName, listTxt); */
 };
 
 exec();
